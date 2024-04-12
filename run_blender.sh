de9e53b298bd6a7b228abece5ee2515cb8c1adb7
Jinhyeok <jinhyeok@unist.ac.kr>
2024-04-12 15:19:39 +0900

    init

---
+++ run_blender.sh
@@ -0,0 +1,6 @@
+blender \
+    --background \
+    --python blender_script.py \
+    -- --object_path /data2/wlsgur4011/objaverse-xl/scripts/rendering/objaverse-xl-test-files/example.obj \
+    --num_renders 12 \
+    --output_dir /data2/wlsgur4011/objaverse-xl/scripts/rendering/output 
